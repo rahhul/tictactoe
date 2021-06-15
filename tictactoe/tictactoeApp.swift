90fa47b50baabd7d706daa11e7e80f08b1dcb316
Rahul K <rahul.koduri@me.com>
2021-06-15 12:12:07 +1000

    Initial Commit

---
+++ tictactoe/tictactoeApp.swift
@@ -0,0 +1,17 @@
+//
+//  tictactoeApp.swift
+//  tictactoe
+//
+//  Created by Rahul on 15/6/21.
+//
+
+import SwiftUI
+
+@main
+struct tictactoeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
